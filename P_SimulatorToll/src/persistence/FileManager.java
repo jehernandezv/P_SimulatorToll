6b62b84918986b43a50d7ac4e08f38d6f887a807
jhonn vega <jhonn.hernandez@uptc.edu.co>
2018-04-22 14:49:18 -0500

    Se modifico una variable estatica en la clase FileMAnager del paquete persistence

--- P_SimulatorToll/src/persistence/FileManager.java
+++ P_SimulatorToll/src/persistence/FileManager.java
@@ -8,11 +8,11 @@ import java.nio.file.Paths;
 
 public class FileManager {
 
-	private static final Path PATH_SEQ_PERSON = Paths.get("plate/seq_plate.txt");
+	private static final Path PATH_SEQ_PLATE = Paths.get("plate/seq_plate.txt");
 
 	public static int getLastId() throws IOException, URISyntaxException{
-		int lastId = Integer.parseInt(Files.readAllLines(PATH_SEQ_PERSON).get(0));
-		Files.write(PATH_SEQ_PERSON, String.valueOf(++lastId).getBytes());
+		int lastId = Integer.parseInt(Files.readAllLines(PATH_SEQ_PLATE).get(0));
+		Files.write(PATH_SEQ_PLATE, String.valueOf(++lastId).getBytes());
 		return lastId;
 	}
 }
